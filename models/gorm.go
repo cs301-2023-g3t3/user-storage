0a8793304da1f1550e336622d943e11ab7bfc9c2
Kevin Chu <kevin@19hours.net>
2023-09-21 10:44:12 +0800

    Update gitignore, remove unnecessary files, add .env support

--- models/gorm.go
+++ models/gorm.go
@@ -3,20 +3,22 @@ package models
 
 import (
 	"fmt"
+	"log"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 )
 
 var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	user := "root"
-	password := ""
-	hostname := "127.0.0.1"
-	port := "3306"
-	dbname := "usersdb"
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	hostname := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, hostname, port, dbname) + "?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
--- router.go
+++ router.go
@@ -14,7 +14,6 @@ import (
 var ginLambda *ginadapter.GinLambda
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// If no name is provided in the HTTP request body, throw an error
 	return ginLambda.ProxyWithContext(ctx, req)
 }
 
