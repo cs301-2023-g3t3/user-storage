20b6c56e28d57acd194adf071a1d528c82b62435
Yao Long <yaolong.tan.2021@smu.edu.sg>
2023-10-24 21:25:14 +0800

    fix: cannot edit user properly :")

--- controllers/user.go
+++ controllers/user.go
@@ -42,14 +42,36 @@ func AddUser(c *gin.Context) {
 }
 
 func EditUser(c *gin.Context) {
-	var user models.User
-	if err := c.BindJSON(&user); err != nil {
-		c.JSON(http.StatusBadRequest, models.HTTPError{
-            Code: http.StatusBadRequest,
+    var user models.User
+    if err := c.BindJSON(&user); err != nil {
+        c.JSON(http.StatusBadRequest, models.HTTPError{
+            Code:    http.StatusBadRequest,
             Message: fmt.Sprintf("Invalid payload. %v", err.Error()),
         })
-		return
-	}
-	models.DB.Save(user)
-	c.IndentedJSON(http.StatusOK, user)
+        return
+    }
+
+    // Check if the user exists in the database
+    existingUser := models.User{}
+    if err := models.DB.Where("id = ?", user.Id).First(&existingUser).Error; err != nil {
+        c.JSON(http.StatusNotFound, models.HTTPError{
+            Code:    http.StatusNotFound,
+            Message: "User not found",
+        })
+        return
+    }
+
+    // Update the user's data
+    models.DB.Model(&existingUser).Updates(user)
+
+    if models.DB.Error != nil {
+        c.JSON(http.StatusInternalServerError, models.HTTPError{
+            Code:    http.StatusInternalServerError,
+            Message: "Failed to update user",
+        })
+        return
+    }
+
+    // Return the updated user
+    c.IndentedJSON(http.StatusOK, existingUser)
 }
