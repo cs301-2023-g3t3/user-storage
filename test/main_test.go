45e1ee144aa51dd5934ee7408ad7900b071fbe88
Yao Long <yaolong.tan.2021@smu.edu.sg>
2023-11-08 00:05:26 +0800

    chore: first test

--- test/main_test.go
+++ test/main_test.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
-	"user-storage/controllers"
 	"user-storage/models"
+	"user-storage/services"
 
 	sqlmock "github.com/DATA-DOG/go-sqlmock"
-	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -32,52 +28,32 @@ func SetUpDB(t *testing.T) (*gorm.DB, sqlmock.Sqlmock){
         t.Fatalf("Error creating GORM DB: %v", err)
     }
 
-    gormDB.AutoMigrate(&models.User{})
-    // Insert the mock data
-    roleVal := uint(1)
-    gormDB.Create(&models.User{
-        Id: "1",
-        FirstName: "John",
-        LastName: "Doe",
-        Email: "john@example.com",
-        Role: &roleVal,
-    })
-
     return gormDB, mock
 }
 
 func TestGetAllUsers(t *testing.T) {
     gormDB, mock := SetUpDB(t)
 
-    userController := controllers.NewUserController(*gormDB)
+    userService := services.NewUserService(gormDB)
 
-    req, err := http.NewRequest("GET", "/users/accounts", nil)
-    if err != nil {
-        t.Fatalf("an error '%s' was not expected while creating request", err)
+    // Define your expected data
+    role := uint(4)
+    expectedUsers := []models.User{
+        {Id: "1", FirstName: "John", LastName: "Doe", Email: "john@example.com", Role: &role},
     }
 
-    recorder := httptest.NewRecorder()
-
-    ctx, _ := gin.CreateTestContext(recorder)
-    ctx.Request = req
-    ctx.Params = gin.Params{}
+    // Define your expected rows and columns
     columns := []string{"id", "first_name", "last_name", "email", "role"}
     expectedRows := sqlmock.NewRows(columns).AddRow("1", "John", "Doe", "john@example.com", 4)
 
-    // Define your expected rows and columns
-    // Set up the mock expectations
+    // Set up the mock expectations for the SQL query
     mock.ExpectQuery("SELECT * FROM `users`").WillReturnRows(expectedRows)
 
-    userController.GetAllUsers(ctx)
-
-    responseBody, err := io.ReadAll(recorder.Body)
-    if err != nil {
-        t.Fatalf("error reading respons body: %v", err)
-    }
-
-    responseBodyStr := string(responseBody)
-    fmt.Println(responseBodyStr)
+    // Call the GetAllUsers method
+    users, statusCode, err := userService.GetAllUsers()
 
     // Assertions
-    assert.Equal(t, http.StatusOK, ctx.Writer.Status()) // Check the HTTP status code
+    assert.NoError(t, err) // Ensure no error occurred
+    assert.Equal(t, http.StatusOK, statusCode) // Check the HTTP status code
+    assert.Equal(t, expectedUsers, *users) // Compare the expected users with the returned users
 }
